854027ca7d008e927feb437b361e2ecc80791eee
Rajibul Islam <154503074+Rajibul029@users.noreply.github.com>
2025-09-10 18:34:48 +0530

    Update db.js

--- backend/models/db.js
+++ backend/models/db.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 dotenv.config();
-const URL=`mysql://root:SQrSAiWBQaqRZchzCqhdKYskUOXZXMik@hopper.proxy.rlwy.net:11496/railway`
+const URL=`mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@hopper.proxy.rlwy.net:11496/${process.env.MYSQLDATABASE}`
 const pool = mysql.createPool(URL);
 
 module.exports = pool;
